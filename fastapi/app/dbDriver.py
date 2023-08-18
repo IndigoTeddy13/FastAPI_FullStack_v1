ce8eff9e85d19472ec7c0e6a857a137a8dc2abf1
IndigoTeddy13 <aragieteddy@gmail.com>
2023-08-18 14:13:33 -0400

    Removed MariaDB
    Focusing on MongoDB right now.
    This commit is distinctly defined in case I need to re-add MariaDB if I ever run into issues I can't resolve with MongoDB.

--- fastapi/app/dbDriver.py
+++ fastapi/app/dbDriver.py
@@ -1,6 +1,5 @@
 import os, json, asyncio #filepaths, JSONs, async requests
 from motor.motor_asyncio import AsyncIOMotorClient #async connections to MongoDB
-from sqlalchemy import create_engine, MetaData
 
 #MongoDB setup
 MONGO_NAME:str = str(os.getenv("MONGODB_INITDB_DATABASE"))
@@ -24,33 +23,3 @@ mongoClient = AsyncIOMotorClient(mongoURL)
 #Test function
 async def testMongo():
     return await mongoClient.list_database_names()
-
-###
-###
-###
-
-#MariaDB setup
-MARIA_NAME:str = str(os.getenv("MARIADB_DATABASE"))
-MARIA_USER:str = str(os.getenv("MARIA_USER"))
-MARIA_PASS:str = str(os.getenv("MARIADB_ROOT_PASSWORD"))
-MARIA_DOMAIN:str = str(os.getenv("MARIA_DOMAIN"))
-portAsStr= str(os.getenv("MARIA_PORT"))
-MARIA_PORT:int = int(portAsStr)
-#Define MariaDB database URL
-#"mysql+pymysql://admin:1234@localhost:3306/dev"
-mariaTemp:str = "mysql+pymysql://{user}:{password}@{domain}:{port}/{dbname}?charset=utf8"
-mariaURL:str = mariaTemp.format(
-    user=MARIA_USER,
-    password=MARIA_PASS,
-    domain=MARIA_DOMAIN,
-    port=MARIA_PORT,
-    dbname=MARIA_NAME
-)
-print(mariaURL)
-#Connect to MariaDB container:
-engine = create_engine(mariaURL)
-metadata = MetaData()
-conn = engine.connect()
-#Test function
-async def testMaria():
-    return metadata.tables.keys()
--- fastapi/app/signinRoute.py
+++ fastapi/app/signinRoute.py
@@ -1,12 +1,11 @@
 import os, json, asyncio #filepaths, JSONs, async requests
 from fastapi import APIRouter, HTTPException #FastAPI stuff
-from .dbDriver import testMongo, testMaria
+from .dbDriver import testMongo
 # MongoDB management route:
 signinRoute = APIRouter()
 
 @signinRoute.get("/")
 async def signinHome()->dict:
     return {
-        "mongo":await testMongo(),
-        "maria":await testMaria()
+        "mongo":await testMongo()
     }
